ffcaa99e7382005051e93e55207aa158939e8566
Ryan Kuchenthal <koochenthal0@gmail.com>
2023-01-09 17:16:45 -0800

    made the Enter key press the convert button by default

--- VolumeConverter/Form1.Designer.cs
+++ VolumeConverter/Form1.Designer.cs
@@ -165,6 +165,7 @@
             // 
             // Form1
             // 
+            this.AcceptButton = this.btnConvert;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.OliveDrab;
